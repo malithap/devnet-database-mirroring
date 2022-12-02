972439b94abf25a19ceed040963f053738a3b5a4
Malitha Perera <malithap@superoffice.com>
2022-12-02 09:18:21 +0530

    Move SoAppId into pipeline variable

---
+++ Source/DatabaseMirroringProject.ArmTemplate/Deploy-Mirroring.ps1
@@ -0,0 +1,81 @@
+﻿#Requires -Version 3.0
+
+Param(
+  [string] $Environment = 'sod',
+  [switch] $ValidateOnly
+)
+
+Write-Output '', 'Start Deploy-Metering'
+
+try {
+  [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(' ', '_'), '3.0.0')
+}
+catch { }
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 3
+
+function Format-ValidationOutput {
+  param ($ValidationOutput, [int] $Depth = 0)
+  Set-StrictMode -Off
+  return @($ValidationOutput | Where-Object { $_ -ne $null } | ForEach-Object { @('  ' * $Depth + ': ' + $_.Message) + @(Format-ValidationOutput @($_.Details) ($Depth + 1)) })
+}
+
+$ResourceGroupLocation = 'Norway East'
+$ResourceGroupName = "rg-mirroring-$($Environment)"
+$TemplateFile = 'Mirroring.bicep'
+$TemplateParametersFile = "Mirroring.$($Environment).parameters.json"
+
+# Generate random SQL admin password. We use Azure AD and MSI to access the data, so admin password is needed only to run db migration
+$SqlAdministratorPassword = "Ph1" + (-join ((35..58) + (65..90) + (97..122) | Get-Random -Count 25 | ForEach-Object {[char]$_}))
+$SecureSqlAdministratorPassword = ConvertTo-SecureString $SqlAdministratorPassword -AsPlainText -Force
+
+$OptionalParameters = New-Object -TypeName Hashtable
+$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
+$TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
+
+# Create the resource group only when it doesn't already exist
+if ($null -eq (Get-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -ErrorAction SilentlyContinue)) {
+  New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop
+}
+
+if ($ValidateOnly) {
+  $ErrorMessages = Format-ValidationOutput (Test-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+      -TemplateFile $TemplateFile `
+      -TemplateParameterFile $TemplateParametersFile `
+      @OptionalParameters)
+  if ($ErrorMessages) {
+    Write-Output '', 'Validation returned the following errors:', @($ErrorMessages), '', 'Template is invalid.'
+   [Environment]::Exit(1)
+  }
+  Write-Output '', 'Template is valid.'
+  Exit 0
+}
+
+$outputs = New-AzResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+  -ResourceGroupName $ResourceGroupName `
+  -TemplateFile $TemplateFile `
+  -environment $Environment `
+  -TemplateParameterFile $TemplateParametersFile `
+  -sqlAdministratorPassword $SecureSqlAdministratorPassword `
+  -Force -Verbose `
+  -ErrorVariable ErrorMessages
+if ($ErrorMessages) {
+  Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
+  [Environment]::Exit(1)
+}
+
+Write-Output '', 'Template is valid.'
+
+$WebAppName = $outputs.Outputs['webAppName'].value
+$ConnectionBase = $outputs.Outputs['baseDatabaseConnectionString'].value
+
+
+# Output from PS
+Write-Host
+Write-Host "Outputs:"
+Write-Host "WebAppName = $WebAppName"
+Write-Host "ConnectionBase = $ConnectionBase"
+Write-Host "About to set outpus variables"
+Write-Host "##vso[task.setvariable variable=WebAppName]$WebAppName"
+Write-Host "##vso[task.setvariable variable=ConnectionBase]$ConnectionBase"
---
+++ Source/DatabaseMirroringProject.ArmTemplate/Mirroring.bicep
@@ -0,0 +1,96 @@
+﻿param location string = resourceGroup().location
+param appHostingPlan string = 'B2'
+param environment string = 'sod'
+@secure()
+param sqlAdministratorPassword string
+param sqlAdministratorSid string
+
+var sqlServerName = 'sql-mirroring-${environment}'
+var sqlAdministratorLogin = 'MirroringSqlAdmin'
+var sqlServerFilewallRulesArray = [
+  {
+    name: 'AllowAllWindowsAzureIps'
+    ipAddress: '0.0.0.0'
+  }
+  {
+    name: 'AllowSuperOfficeOld'
+    ipAddress: '217.144.239.130'
+  }
+  {
+    name: 'AllowSuperOffice'
+    ipAddress: '195.1.42.202'
+  }
+  {
+    name: 'AllowSuperOfficeWifi'
+    ipAddress: '91.123.49.64'
+  }
+]
+
+var baseDatabaseConnectionString = 'Server=tcp:${sqlServerName}${az.environment().suffixes.sqlServerHostname},1433;User ID=${sqlAdministratorLogin};Password=${sqlAdministratorPassword}'
+var webAppHostingPlanName = 'plan-mirroring-${environment}'
+var webAppName = 'app-mirroring-${environment}'
+
+// SQL server
+resource sqlServer 'Microsoft.Sql/servers@2021-11-01' = {
+  name: sqlServerName
+  location: location
+  tags: {
+    displayName: 'SQL Server'
+  }
+  properties: {
+    administratorLogin: sqlAdministratorLogin
+    administratorLoginPassword: sqlAdministratorPassword
+    version: '12.0'
+    publicNetworkAccess: 'Enabled'
+  }
+}
+
+resource sqlServerAdmin 'Microsoft.Sql/servers/administrators@2021-11-01' = {
+  parent: sqlServer
+  name: 'ActiveDirectory'
+  properties: {
+    administratorType: 'ActiveDirectory'
+    login: 'aad_${sqlAdministratorLogin}'
+    sid: sqlAdministratorSid
+  }
+}
+
+resource sqlServerFilewallRules 'Microsoft.Sql/servers/firewallRules@2021-11-01' = [for sqlServerFilewallRule in sqlServerFilewallRulesArray: {
+  parent: sqlServer
+  name: sqlServerFilewallRule.name
+  properties: {
+    startIpAddress: sqlServerFilewallRule.ipAddress
+    endIpAddress: sqlServerFilewallRule.ipAddress
+  }
+}]
+
+resource webAppHostingPlan 'Microsoft.Web/serverfarms@2022-03-01' = {
+  name: webAppHostingPlanName
+  location: location
+  tags: {
+    displayName: 'Web App Hosting Plan'
+  }
+  sku: {
+    name: appHostingPlan
+  }
+}
+
+resource webApp 'Microsoft.Web/sites@2022-03-01' = {
+  name: webAppName
+  location: location
+  tags: {
+    'hidden-related:${resourceGroup().id}/providers/Microsoft.Web/serverfarms/${webAppHostingPlanName}': 'Resource'
+    displayName: '${webAppName} Web App'
+  }
+  properties: {
+    serverFarmId: webAppHostingPlan.id
+    httpsOnly: true
+  }
+  identity: {
+    type: 'SystemAssigned'
+  }
+}
+
+output webAppName string = webAppName
+output baseDatabaseConnectionString string = baseDatabaseConnectionString
+
